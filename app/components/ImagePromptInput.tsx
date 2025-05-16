451c4bcb90afd373a2ff5ad4f8231c500bac80e1
AvulaRamSwaroop <avularamswaroop@gmail.com>
2025-05-17 03:09:58 +0530

    deploy

--- app/components/ImagePromptInput.tsx
+++ app/components/ImagePromptInput.tsx
@@ -77,6 +77,7 @@ export function ImagePromptInput({
           ))}
         </select>
       </div>
+
       <div className="space-y-2">
         <p className="text-md font-bold text-black">
           {isEditing
